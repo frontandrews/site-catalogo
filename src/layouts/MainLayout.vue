8b208130312841a0183a201a0fe6ff8b51ca6edd
AndrewsRGomes <andrews.ribeiro.gomes@gmail.com>
2020-09-29 16:34:21 -0300

    First Commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,89 @@
+<template>
+<q-layout view="lHh Lpr lFf">
+
+    <q-page-container class="text-justify">
+        <transition
+          appear
+          duration-enter='1.5'
+          duration-leave='2'
+          enter-active-class='animated fadeIn'
+          leave-active-class='animated fadeOut'
+        >
+          <router-view />
+        </transition>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d918.8867324954912!2d-43.11999227080999!3d-22.893189299063643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9983c3cf7d2745%3A0xf1f2950c77a3a41f!2sR.%20Luiz%20Leopoldo%20Fernandes%20Pinheiro%2C%20521%20-%20Centro%2C%20Niter%C3%B3i%20-%20RJ%2C%2024030-125!5e0!3m2!1sen!2sbr!4v1601164624876!5m2!1sen!2sbr" width="100%" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+    </q-page-container>
+
+    <q-footer class="absolute-bottom bg-dark q-pa-xl">
+          <div class="container">
+            <div class="row">
+              <div class="col-6">
+                <q-list>
+                <q-item>
+                    <q-item-section>Contato</q-item-section>
+                  </q-item>
+                  <q-item>
+                    <q-item-section>Email: Comercial@marccomercio.com.br</q-item-section>
+                  </q-item>
+                  <q-item>
+                    <q-item-section>Whatsapp: 21 97077-9851</q-item-section>
+                  </q-item>
+                </q-list>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-6">
+                <q-list>
+                  <q-item>
+                    <q-item-section>Endereço: RJ, Niterói, Centro, Rua Luís Leopoldo Fernandes Pinheiro, 521 - 2º Andar - 24030-121</q-item-section>
+                  </q-item>
+                  <q-item>
+                    <q-item-section>Endereço: RJ, Parada de Lucas - Rua Saracá, 5 (Depósito)</q-item-section>
+                  </q-item>
+                  <q-item>
+                    <q-item-section style="display: inline">Telefone: <a  class="link_style" target="_blank" href="+21964255128">(21) 96425-5128</a></q-item-section>
+                  </q-item>
+                </q-list>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-6 q-pa-sm absolute-bottom" style="font-size: 10px">
+                @ Todos os direitos reservados - MARC COMÉRCIO DE MATERIAIS, TECNOLOGIA E SERVIÇOS LTDA
+                - Created by <a class="link_style" target="_blank" href="https://www.frontandrews.com.br">FrontAndrews</a>
+              </div>
+            </div>
+
+          </div>
+      </q-footer>
+
+</q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  methods: {
+    goBackHome () {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.link_style {
+  color: white;
+  text-decoration: none;
+  text-transform: uppercase;
+}
+
+.link_style:hover {
+  color: rgb(209, 202, 202);
+  text-decoration: none;
+  text-transform: uppercase;
+}
+
+.logo:hover {
+  color: grey;
+  cursor: pointer
+}
+
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="q-pa-none bg-dark">
+    <q-carousel
+      animated
+      v-model="slide"
+    >
+
+      <q-carousel-slide :name="1" img-src="~assets/images/carousel-2.jpg">
+        <div class="absolute-center custom-caption text-center">
+          <div class="text-h2 q-pa-md bg-red text-white">Marc Comércio</div>
+          <div class="text-subtitle1 text-white">COMÉRCIO DE MATERIAIS, TECNOLOGIA E SERVIÇOS </div>
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
+  <div class="row q-ma-md" id="catalogo">
+
+    <q-card class="col-xs-12 col-sm-6 my-card catalog-item rounded q-pa-xs bg-dark"  v-for="(item, index) in items" :key="index">
+      <a :href="item.download" target='_blank'>
+      <q-img
+        :src="item.img"
+        :height="200"
+        class="imagem-catalog"
+      />
+
+      <div class="text-h5 absolute-top text-subtitle2 text-center text-white">
+         <div class="text-h6 q-pt-lg title absolute-right"><span class="q-pa-md bg-blue q-mr-md">{{ item.title }}</span></div>
+        <!-- <div class="subtitle">{{ item.subtitle }}</div> -->
+      </div>
+        </a>
+    </q-card>
+
+  </div>
+
+    <div class="row text-center">
+      <div class="col-xs-12 col-sm-12  col-md-6 bg-dark text-white q-pa-xs q-py-md">
+        <div class="text-h4 q-mb-md" style="align:center">Quem Somos</div>
+        <p class="q-mx-md  text-justify subtitle">Nossa equipe é formada por profissionais experientes, com anos de vivência no mercado de comércio e distribuição de produtos, para atender nossos clientes com excelência e de maneira exclusiva.</p>
+      </div>
+      <div class="col-xs-12 col-sm-12  col-md-6 q-pa-xs  q-py-md bg-white">
+        <div class="text-h4 q-mb-md" style="align:center">Nossos Valores</div>
+       <p class="q-mx-md  text-justify subtitle"><b>Missão:</b>
+Oferecer soluções em produtos com excelência, agregando valor e satisfação aos nossos Clientes, Fornecedores, Colaboradores e Comunidade.</p>
+       <p class="q-mx-md  text-justify subtitle"><b>Visão:</b>
+Buscar o reconhecimento nacional como parceiros valiosos para os nossos clientes, fornecedores, colaboradores e comunidade.</p>
+        <p class="q-mx-md  text-justify subtitle"><b>Valores:</b> Ética, Comprometimento, Parcerias Estratégicas, Compromisso com Qualidade, Amizade e respeito.
+</p>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      slide: 1,
+      autoplay: true,
+      items: [
+        { title: 'Informática', subtitle: '', img: '/images/informatica.jpg', download: '/pdf/Informática.pdf' },
+        { title: 'Ferramentas', subtitle: '', img: '/images/ferramentas.jpg', download: '/pdf/Ferramentas.pdf' },
+        { title: 'Positivo', subtitle: '', img: '/images/positivo.png', download: '/pdf/Positivo.pdf' },
+        { title: 'Vaio', subtitle: '', img: '/images/vaio.jpg', download: '/pdf/Vaio.pdf' }
+      ]
+    }
+  },
+  computed: {
+    updatedSrc () {
+      if (process.env.MODE === 'electron') {
+        return '/' + this.src
+      }
+      return this.src
+    }
+  }
+}
+</script>
+
+<style scoped>
+* {
+  font-family: 'Arial';
+}
+.catalog-item:hover {
+  opacity: 0.80;
+  cursor: pointer;
+
+}
+
+.imagem-catalog {
+  height:300px;
+}
+
+.title {
+  font-family: 'roboto';
+  font-weight: bold;
+}
+
+* {
+  font-family: 'roboto';
+  font-weight: bold;
+}
+
+.subtitle {
+  font-family: 'segoe ui light';
+  font-weight: bold;
+}
+</style>
